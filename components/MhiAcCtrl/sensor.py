fc039ab9d2177515154a8a36ec18c4f061c2005b
LawnMeower <sergehill@gmail.com>
2025-02-27 20:51:26 +0100

    DEVICE_CLASS_TEMPERATURE added

--- components/MhiAcCtrl/sensor.py
+++ components/MhiAcCtrl/sensor.py
@@ -3,6 +3,7 @@ import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import (
     DEVICE_CLASS_FREQUENCY,
+    DEVICE_CLASS_TEMPERATURE,
     STATE_CLASS_MEASUREMENT,
     STATE_CLASS_TOTAL_INCREASING,
     UNIT_CELSIUS,
